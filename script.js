192f707e2865726b58df497d14ce0d83351ee9c6
Marcelee Laylay <youmustbeajoke@gmail.com>
2022-11-22 15:26:45 +0800

    Added the skeleton of HTML/CSS and JS and UI

---
+++ script.js
@@ -0,0 +1,18 @@
+const Formatter = (function () {
+    const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
+
+    const makeUppercase = (text) => {
+        log("Making uppercase");
+        return text.toUpperCase();
+    };
+
+    return {
+        makeUppercase,
+    }
+})();
+
+
+let x = Formatter;
+console.log(typeof x);
+
+
