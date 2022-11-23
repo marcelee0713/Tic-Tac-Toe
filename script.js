41ef0ada23b01d51720decdde978971b09d8a3ab
Marcelee Laylay <youmustbeajoke@gmail.com>
2022-11-23 22:53:14 +0800

    Did a minimal functionality of the gameboard

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
-const Formatter = (function () {
-    const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
-
-    const makeUppercase = (text) => {
-        log("Making uppercase");
-        return text.toUpperCase();
-    };
-
-    return {
-        makeUppercase,
+const players = (name, marker) => {
+    return{
+        name, marker
     }
+}
+
+const game = (() => {
+    let playerMode = document.getElementById("btn-playermode");
+    let restart = document.getElementById("btn-restart");
+
+    let defaultMode = "PLAYER"
+    let player1Turn = true;
+    let turns = document.getElementById("turns");
+
+    if(defaultMode === "PLAYER"){
+        playerMode.style.backgroundColor = "black";
+        playerMode.style.color = "#DEDEDE"
+    }
+
+    const player1 = players("PLAYER1", "X");
+    const player2 = players("PLAYER2", "O");
+
+    const gameboard = document.querySelectorAll("#game");
+    gameboard.forEach(pressedSquare => pressedSquare.addEventListener("click", (e) => {
+        //console.log(e.target.getAttribute("value"));
+        if(!e.target.hasChildNodes()){
+            if(player1Turn){
+                let createMarker = document.createElement("span");
+                createMarker.textContent = player1.marker;
+                e.target.appendChild(createMarker);
+                turns.textContent = `${player2.name}'S TURN`
+                player1Turn = false;
+            }
+            else{
+                let createMarker = document.createElement("span");
+                createMarker.textContent = player2.marker;
+                e.target.appendChild(createMarker);
+                turns.textContent = `${player1.name}'S TURN`
+                player1Turn = true;
+            }
+        }
+    }));
 })();
-
-
-let x = Formatter;
-console.log(typeof x);
-
 
