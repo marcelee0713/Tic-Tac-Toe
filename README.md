ffcfdae5a19f7e3393443839b6532fd11f12f42c
Marcelee Laylay <youmustbeajoke@gmail.com>
2022-11-26 14:59:59 +0800

    Finished the player mode

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Tic-Tac-Toe
 A TOP Project
+
+Kicco2002 is me "marcelee" just had an issue configuring my git on my laptop. Because of the previous hackathon.
--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const players = (name, marker) => {
-    return{
+    return {
         name, marker
     }
 }
 
 const game = (() => {
-    let playerMode = document.getElementById("btn-playermode");
-    let restart = document.getElementById("btn-restart");
+    const playerMode = document.getElementById("btn-playermode");
+    const restart = document.getElementById("btn-restart");
 
     let defaultMode = "PLAYER"
     let player1Turn = true;
     let turns = document.getElementById("turns");
-    let turnCounts = 9;
+    let turnCounts = 8;
 
     let player1Numbers = [];
     let player2Numbers = [];
 
-    if(defaultMode === "PLAYER"){
+    if (defaultMode === "PLAYER") {
         playerMode.style.backgroundColor = "black";
         playerMode.style.color = "#DEDEDE"
     }
@@ -26,9 +26,9 @@ const game = (() => {
 
     const gameboard = document.querySelectorAll("#game");
     gameboard.forEach(pressedSquare => pressedSquare.addEventListener("click", (e) => {
-        //console.log(e.target.getAttribute("value"));
-        if(!e.target.hasChildNodes()){
-            if(player1Turn){
+        //can't press the same box if it already have one
+        if (!e.target.hasChildNodes()) {
+            if (player1Turn) {
                 let createMarker = document.createElement("span");
                 createMarker.textContent = player1.marker;
                 e.target.appendChild(createMarker);
@@ -36,9 +36,10 @@ const game = (() => {
                 player1Turn = false;
                 let pick = parseInt(e.target.getAttribute("value"));
                 player1Numbers.push(pick);
-                gameBoard.compareNumbers(player1Numbers);
+                gameBoard.compareNumbers(player1Numbers, player1.name);
+                gameBoard.reduceTurnCounts(turnCounts--);
             }
-            else{
+            else {
                 let createMarker = document.createElement("span");
                 createMarker.textContent = player2.marker;
                 e.target.appendChild(createMarker);
@@ -46,54 +47,90 @@ const game = (() => {
                 player1Turn = true;
                 let pick = parseInt(e.target.getAttribute("value"));
                 player2Numbers.push(pick);
-                gameBoard.compareNumbers(player2Numbers);
+                gameBoard.compareNumbers(player2Numbers, player2.name);
+                gameBoard.reduceTurnCounts(turnCounts--);
             }
         }
     }));
 
-    return{
+    const reset = () => {
+        turnCounts = 8;
+        turns.textContent = `${player1.name}'S TURN`
+        player1Turn = true;
+    }
+
+    return {
         player1, player1Numbers,
         player2, player2Numbers,
-        turnCounts, player1Turn
+        turnCounts, player1Turn, 
+        restart, gameboard,
+        reset
     }
 })();
 
 const gameBoard = (() => {
     const winningConditions = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
     ];
-    /*
-    //TODO match the player 1 or 2 playerNumbers.
-    for(let i = 0; i < winningConditions.length; i++){
-        //console.log(winningConditions[i][j].includes());
-        let currentArray = winningConditions[i];
-        console.log(currentArray);
-        if(currentArray.includes(game.player1Numbers)){
-            
-        }
-    }
-    */
-   const compareNumbers = (pNumbers) => {
-    for(let i = 0; i < winningConditions.length; i++){
-        //console.log(winningConditions[i][j].includes());
-        let currentWinConArray = winningConditions[i];
-        let playerCurrentArray = pNumbers;
-        //console.log(playerCurrentArray.includes(currentWinConArray));
 
-        if(currentWinConArray.every(ai => playerCurrentArray.includes(ai))){
-            console.log(playerCurrentArray);
-            console.log(currentWinConArray);
+    //for modal
+    const modal = document.getElementById("winner-modal");
+    const modalHeader = document.getElementById("header");
+    const tryAgainBtn = document.getElementById("try-again-btn");
+
+
+    //will check if either the players contain each array of the winning conditions
+    const compareNumbers = (pNumbers, pName) => {
+        for (let i = 0; i < winningConditions.length; i++) {
+            let currentWinConArray = winningConditions[i];
+            let playerCurrentArray = pNumbers;
+
+            //if the player's array matches any of the winning conditions. player wins
+            if (currentWinConArray.every(ai => playerCurrentArray.includes(ai))) {
+                modal.style.display = "flex";
+                modalHeader.textContent = `${pName} WINS!`;
+            }
+        }
+
+    }
+
+    const reduceTurnCounts = count => {
+        game.turnCounts = count;
+        if(count === 0){
+            modal.style.display = "flex";
+            modalHeader.textContent = "TIE!"
         }
     }
-   }
-   return{
-    compareNumbers
-   }
+
+    const restartGame = () => {
+        game.player1Numbers.length = 0;
+        game.player2Numbers.length = 0;
+        game.reset();
+        modal.style.display = "none";
+        modalHeader.textContent = ""
+        game.gameboard.forEach(squares => {
+            let parentBox = squares.children; 
+            for(let i = 0; i < squares.children.length; i++){
+                if(parentBox[i].hasChildNodes()){
+                    while(parentBox[i].firstChild){
+                        parentBox[i].removeChild(parentBox[i].firstChild);
+                    }
+                }
+            }
+        });
+    }
+
+    tryAgainBtn.addEventListener("click", restartGame);
+    game.restart.addEventListener("click", restartGame);
+
+    return {
+        compareNumbers, reduceTurnCounts
+    }
 })()
